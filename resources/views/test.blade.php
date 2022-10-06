8c831f12539be2b4b6158d42055dd1d2e54a4339
watsoningosi <watstarsparta@gmail.com>
2022-10-06 22:49:08 +0300

    laravel pilot comit

---
+++ resources/views/test.blade.php
@@ -0,0 +1,20 @@
+ <!DOCTYPE html>
+ <html lang="en">
+
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <title>Test</title>
+ </head>
+
+ <body>
+     <h1> Test</h1>
+
+     <h4> {{ $name }}</h4>
+     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit iusto accusamus et, modi animi iure obcaecati
+         ullam quas similique eligendi veniam excepturi optio quibusdam nesciunt eveniet? Sunt sed delectus porro! </p>
+
+ </body>
+
+ </html>
