7b11300a4e9f9d59254949433a88b7ecc0b5bb8f
watsoningosi <watstarsparta@gmail.com>
2022-10-06 23:14:02 +0300

    Routing Wild cards

--- routes/web.php
+++ routes/web.php
@@ -12,23 +12,41 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 
-
+1
 Route::get('/', function () {
     return view('welcome');
 });
+2
 Route::get('/welcome', function () {
     return view('welcome');
 });
+3
  Route::get('test', function () {
     return view('test');
 });
-*/
-
-
+4
 Route::get('/', function () {
     $name = request('name');
 
  return view('test', [
     'name' => $name
  ]);
+});
+*/
+
+Route::get('/posts/{post}', function ($post) {
+    //data store
+    $posts = [
+        '1' => 'hello to discovery',
+        '2' => 'peanut humper episode yeah',
+        '3' => 'cyril figus , jerry smith'
+
+    ];
+    if (!array_key_exists($post, $posts)) {
+        abort(404, 'No post found');
+    }
+
+    return view('post', [
+        'post' => $posts[$post]
+    ]);
 });
